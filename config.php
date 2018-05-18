99c8d329d162277f0addfebd5b8fc725c3352de7
David Barratt <dbarratt@wikimedia.org>
2018-05-18 16:10:45 +0200

    Create default configuration

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+// @TODO Remove this!
+ini_set('opcache.enable', 0);
+
+const MW_CONFIG_FILE =  __DIR__. '/LocalSettings.php';
+const MW_VENDOR_DIR = __DIR__ . '/vendor/';
+const MW_CONFIG_CALLBACK = 'mwProjectDefaultConfig';
+
+function mwProjectDefaultConfig() {
+  // Pull all globals into the function's scope.
+  extract($GLOBALS, EXTR_REFS);
+
+  // Set the project default values.
+  $wgResourceBasePath = '/core/';
+  $wgExtensionDirectory = __DIR__ . '/html/extensions/';
+  $wgExtensionAssetsPath = '/extensions/';
+  $wgStyleDirectory = __DIR__ . '/html/skins/';
+  $wgStylePath = '/skins';
+
+  // If the config file is unreadable, throw the standard error.
+  if ( !is_readable( MW_CONFIG_FILE ) ) {
+    wfWebStartNoLocalSettings();
+  }
+
+  require_once MW_CONFIG_FILE;
+}
---
+++ html/load.php
@@ -0,0 +1,5 @@
+<?php
+
+require '../config.php';
+
+require 'core/load.php';
